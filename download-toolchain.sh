b08da57b2557adf725b450890a906d555b0b8601
Daniel Cordero <me@xxoo.ws>
2015-02-22 15:07:30 +0000

    Remove multicore simulator fetching
    The script downloads this anyway, as part of toolchain-components.

--- download-toolchain.sh
+++ download-toolchain.sh
@@ -74,14 +74,6 @@
 #     infrastructure URL, which may be changed by the --infra-url option. By
 #     default all components are downloaded.
 
-# --multicore-sim | --no-multicore-sim
-
-#     Download, or (with the --no- prefix) don't download the GDB based
-#     experimental multi-core simulator.
-#     The components are downloaded from the
-#     infrastructure URL, which may be changed by the --infra-url option. By
-#     default all components are downloaded.
-
 
 # --help | -h
 
@@ -360,7 +352,6 @@ do_mpc="--do-mpc"
 do_isl="--do-isl"
 do_cloog="--do-cloog"
 do_ncurses="--do-ncurses"
-do_multicore_sim="--multicore-sim"
 
 until
 opt=$1
@@ -427,10 +418,6 @@ case ${opt} in
 	do_ncurses="$1"
 	;;
 
-    --multicore-sim | --no-multicore-sim)
-	do_multicore_sim="$1"
-	;;
-
     ?*)
 	echo "Usage: ./download-toolchain [--force | --no-force]"
 	echo "                            [--clone | --download]"
@@ -442,7 +429,6 @@ case ${opt} in
 	echo "                            [--isl | --no-isl]"
 	echo "                            [--cloog | --no-cloog]"
 	echo "                            [--ncurses | --no-ncurses]"
-	echo "                            [--multicore-sim | --no-multicore-sim]"
 	echo "                            [--help | -h]"
 	exit 1
 	;;
